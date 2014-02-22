a45866c34f5e7b94d350f3e91290904df23ec8b8
Simon Waldherr <git@simon.waldherr.eu>
2014-02-22 21:01:38 +0100

    Update toggleMenu.js (js(h|l)int)

--- app/scripts/directives/toggleMenu.js
+++ app/scripts/directives/toggleMenu.js
@@ -1,8 +1,9 @@
 // http://geniuscarrier.com/ng-toggle-in-angularjs/
 angular.module('snotes20App', [])
 	.controller('AppCtrl',['$scope', function($scope){
+		'use strict';
 		$scope.menu = true;
 		$scope.toggleMenu = function() {
 			$scope.menu = $scope.menu === false ? true: false;
 		};
-}]);
+	}]);
