fd3cc6aedd9b3377b3d1cefbafc2367c816a9e0f
luto <m@luto.at>
2014-06-10 19:41:16 +0200

    importer: just overwrite when updating, keep some attributes

--- snotes20/management/commands/importexternal.py
+++ snotes20/management/commands/importexternal.py
@@ -11,30 +11,43 @@ import snotes20.models as models
 logger = logging.getLogger(__name__)
 
 
+def podcast_merger(dbentry, entry):
+    entry.create_date = dbentry.create_date
+
+
+def episode_merger(dbentry, entry):
+    entry.document = dbentry.document
+    entry.create_date = dbentry.create_date
+    entry.number = dbentry.number
+
+
 def import_from_source(source):
     logger.info("downloading Podcasts")
     podcasts = source.get_podcasts()
 
-    logger.info("downloading Episodes")
-    yesterday = (datetime.date.today() - datetime.timedelta(1))
-    tomorrow = (datetime.date.today() + datetime.timedelta(1))
-    episodes = source.get_episodes(yesterday, tomorrow)
-
     with transaction.atomic():
         logger.info("importing Podcasts")
-        import_thing(source, podcasts, models.Podcast.objects)
+        import_thing(source, podcasts, podcast_merger, models.Podcast.objects)
+
+        logger.info("downloading Episodes")
+        yesterday = (datetime.date.today() - datetime.timedelta(1))
+        tomorrow = (datetime.date.today() + datetime.timedelta(1))
+        episodes = source.get_episodes(yesterday, tomorrow)
 
         logger.info("importing Episodes")
-        import_thing(source, episodes, models.Episode.objects.filter(document=None))
+        import_thing(source, episodes, episode_merger, models.Episode.objects.filter(document=None))
 
 
-def import_thing(source, data, oqry):
+def import_thing(source, data, merger, oqry):
     for entry in data:
         qry = oqry.filter(source_id=entry.source_id).filter(source=source.shortname)
 
         if qry.exists():
             dbentry = qry.get()
             logger.debug("updating {}".format(dbentry))
+            entry.id = dbentry.id
+            merger(dbentry, entry)
+            entry.save()
         else:
             logger.debug("creating {}".format(entry))
             entry.save()
