742c6013224b516ebfd31149c01c791bfeb8627d
luto <m@luto.at>
2014-06-10 08:17:27 +0200

    Initial commit
    mostly generator-code
    fixed html5Mode in Gruntfile.js

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name snotes30App
+ * @description
+ * # snotes30App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('snotes30App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name snotes30App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the snotes30App
+ */
+angular.module('snotes30App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
