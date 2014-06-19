bae8fe3bc5ca72f71d4da621e52355334cedc822
luto <m@luto.at>
2014-06-19 18:08:36 +0200

    login-form: only show fields for the current mode (register/login)

--- app/scripts/controllers/sidebar.js
+++ app/scripts/controllers/sidebar.js
@@ -1,10 +1,48 @@
 'use strict';
 
 angular.module('snotes30App')
-  .controller('SidebarController', function ($scope) {
+  .controller('SidebarController', function ($scope, AuthenticationService) {
 
     $scope.loginform = {
-      isDown: false
+      isDown: false,
+      mode: 'login'
     };
+
+    $scope.errors = {};
+
+    function flipFormMode() {
+      $scope.loginform.mode = $scope.loginform.mode === 'login' ? 'register' : 'login';
+    }
+
+    $scope.login = function () {
+      if($scope.loginform.mode === 'register') {
+        flipFormMode();
+      } else {
+        doLogin();
+      }
+    };
+
+    function doLogin() {
+      AuthenticationService.login(
+        $scope.username,
+        $scope.password
+      );
+    }
+
+    $scope.register = function () {
+      if($scope.loginform.mode === 'login') {
+        flipFormMode();
+      } else {
+        doRegister();
+      }
+    };
+
+    function doRegister() {
+      AuthenticationService.register(
+        $scope.username,
+        $scope.email,
+        $scope.password
+      );
+    }
 
   });
---
+++ app/scripts/services/authentication.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('snotes30App')
+  .service('AuthenticationService', function () {
+    this.login = function (username, password) {
+
+    };
+
+    this.register = function (username, email, password) {
+
+    };
+  });
