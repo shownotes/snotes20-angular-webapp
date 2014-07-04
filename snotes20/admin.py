4a63ad9ef5b914d8dd7f8ec1117a0c4fa5f43718
luto <m@luto.at>
2014-07-04 10:26:46 +0200

    add user, podcast, episode, publicaton, publicationrequest to admin

--- snotes20/admin.py
+++ snotes20/admin.py
@@ -1,1 +1,23 @@
 from django.contrib import admin
+
+import snotes20.models as models
+
+@admin.register(models.Podcast)
+class PodcastAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Episode)
+class EpisodeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.PublicationRequest)
+class PublicationRequestAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.NUser)
+class NUserAdmin(admin.ModelAdmin):
+    pass
