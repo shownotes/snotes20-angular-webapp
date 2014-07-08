a99533bb68e2a28b7ccfd609dd7f1697bb1aac68
luto <m@luto.at>
2014-07-08 13:28:12 +0200

    profile: color+bio

--- app/scripts/controllers/user/profile.js
+++ app/scripts/controllers/user/profile.js
@@ -2,7 +2,22 @@
 
 angular.module('snotes30App')
   .controller('UserProfileCtrl', function ($scope, Restangular) {
-    $scope.user = {
-      username: 'Gurkenluto'
+    var me = Restangular.one('users', 'me');
+
+    $scope.user = { };
+
+    var reloadMe = function () {
+      me.get().then(function (user) {
+        $scope.user = user;
+      });
+    };
+
+    reloadMe();
+
+    $scope.saveBioColor = function () {
+      me.patch({
+        'color': $scope.user.color.substr(1).toUpperCase(),
+        'bio': $scope.user.bio
+      }).then(reloadMe);
     };
   });
