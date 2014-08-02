56787a71127e577576862251e9adfa45ee2cb44d
luto <m@luto.at>
2014-08-02 09:09:12 +0200

    OSFDocumentState: osf_str impl

--- snotes20/models/state/osf.py
+++ snotes20/models/state/osf.py
@@ -1,12 +1,17 @@
 from django.db import models
 
-from uuidfield import UUIDField
+from osf import OSFLine
 
 from .DocumentState import DocumentState
 
 
 class OSFDocumentState(DocumentState):
-    pass
+
+    def to_osf_str(self):
+        return "\n".join([note.to_osf_str() for note in self.notes])
+
+    def __str__(self):
+        return "OSFDocumentState, " + self.notes.count() + " notes"
 
 
 class OSFTag(models.Model):
@@ -23,6 +28,18 @@ class OSFNote(models.Model):
     text = models.CharField(max_length=300)
     link = models.URLField(null=True)
     tags = models.ManyToManyField(OSFTag, related_name="notes")
+
+    def __str__(self):
+        return str(self.to_OSFLine())
+
+    def to_OSFLine(self):
+        line = OSFLine()
+        line.time = self.time
+        line.indentation = self.indentation
+        line.text = self.text
+        line.link = self.link
+        line.tags = [tag.short for tag in self.tags]
+        return line
 
     class Meta:
         unique_together = ('order', 'state')
