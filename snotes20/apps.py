7ae956c24a38020624e39a15a1c5a6d4f1c4163c
luto <m@luto.at>
2014-08-17 10:43:50 +0200

    put signal handling for create/delete doc into a AppConfig

---
+++ snotes20/apps.py
@@ -0,0 +1,22 @@
+from django.apps import AppConfig
+from django.db.models.signals import post_save, post_delete
+
+import snotes20.models as models
+import snotes20.editors as editors
+
+
+class DefaultConfig(AppConfig):
+    name = 'snotes20'
+
+    def ready(self):
+        def editor_create_doc(sender, instance, created, **kwargs):
+            if created:
+                editor = editors.EditorFactory.get_editor(instance.editor)
+                editor.create_document(instance)
+
+        def editor_delete_doc(sender, instance, **kwargs):
+            editor = editors.EditorFactory.get_editor(instance.editor)
+            editor.delete_document(instance)
+
+        post_save.connect(editor_create_doc, sender=models.Document, dispatch_uid='editor_create_doc')
+        post_delete.connect(editor_delete_doc, sender=models.Document, dispatch_uid='editor_delete_doc')
