ac4fe618fa62b35cd7aa43da07c932cea9d39a0f
luto <m@luto.at>
2014-08-17 14:37:07 +0200

    forward chat-messages via socket.io

--- handlers/DOCUMENT_CHAT_MESSAGE.js
+++ handlers/DOCUMENT_CHAT_MESSAGE.js
@@ -1,6 +1,8 @@
 var winston = require('winston');
+var sockets = require('../sockets.js');
 
 exports.exchange = 'DOCUMENT_CHATMESSAGE';
 exports.handle = function (msg, content) {
     winston.info('chat message:', content);
+    sockets.emit(content.document, 'DOCUMENT_CHATMESSAGE', content);
 };
