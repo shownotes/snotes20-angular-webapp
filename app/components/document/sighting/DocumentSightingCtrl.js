a74597dc24818c01a9d809e8887a22404ac45da1
luto <m@luto.at>
2014-11-05 21:07:22 +0100

    sighting: editor/preview switch

--- app/components/document/sighting/DocumentSightingCtrl.js
+++ app/components/document/sighting/DocumentSightingCtrl.js
@@ -4,4 +4,10 @@ angular.module('snotes30App')
 .controller('DocumentSightingCtrl', function ($scope, $rootScope, doc, DocumentService) {
   $scope.doc = doc;
   $scope.episode = doc.episode;
+
+  $scope.editorMode = 'preview';
+
+  $scope.switchEditorMode = function (mode) {
+    $scope.editorMode = mode;
+  };
 });
