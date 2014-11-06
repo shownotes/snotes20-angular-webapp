d0e27624604adebb4d595a6a7c4f41b8624deda1
luto <m@luto.at>
2014-11-06 22:07:40 +0100

    fix grunt watch for JS files

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,7 +67,7 @@ module.exports = function (grunt) {
         tasks: ['wiredep']
       },
       js: {
-        files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
+        files: ['<%= yeoman.app %>/components/{,*/}*.js'],
         tasks: ['newer:jshint:all'],
         options: {
           livereload: '<%= connect.options.livereload %>'
