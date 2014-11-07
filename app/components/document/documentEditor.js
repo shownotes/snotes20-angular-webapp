44e1592aac7c547d23d640921848bb5e7d21c0c1
luto <m@luto.at>
2014-11-07 17:10:30 +0100

    fix editor in sighting view
    replace on directives tries to merge the replaced element with the
    directive. When it finds two attributes with the same name it concats
    the values with a space in between. This doesn't work at all with ng-if,
    which is used by the sighting-view.. aaahhrrngular.
    also fixes the width of the iframe

--- app/components/document/documentEditor.js
+++ app/components/document/documentEditor.js
@@ -4,11 +4,10 @@ angular.module('snotes30App')
   .directive('documentEditor', function() {
     return {
       restrict: 'E',
-      replace: true,
       scope: {
         document: '='
       },
-      template: '<iframe ng-if="docurl" src="{{docurl}}"></iframe>',
+      template: '<iframe class="documentIframe" ng-if="docurl" src="{{docurl}}"></iframe>',
       controller: function ($scope, $sce, DocumentService) {
         DocumentService.getEditor($scope.document.editor).then(function (editor) {
           $scope.docurl = $sce.trustAsResourceUrl(editor.url + '/' + $scope.document.urlname);
