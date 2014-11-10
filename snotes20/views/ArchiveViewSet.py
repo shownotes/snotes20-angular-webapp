ea559b8f16a951b4adacfcb4e048794487257f16
luto <m@luto.at>
2014-11-10 20:20:06 +0100

    proper archive api (only return pods with episodes, add 'recent' filteR)

--- snotes20/views/ArchiveViewSet.py
+++ snotes20/views/ArchiveViewSet.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.db.models import Max
 
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -8,8 +9,16 @@ import snotes20.serializers as serializers
 
 class ArchiveViewSet(viewsets.ViewSet):
     def list(self, request):
-        pods = models.Podcast.objects.all()
+        qry = models.Podcast.objects.filter(episodes__publications__isnull=False)
 
-        data = serializers.PodcastSerializer(pods, many=True).data
+        type = 'all'
+
+        if 'type' in request.QUERY_PARAMS:
+            type = request.QUERY_PARAMS['type']
+
+        if type == 'recent':
+            qry = qry.order_by('-episodes__publications__create_date')[:15]
+
+        data = serializers.PodcastSerializer(qry, many=True).data
 
         return Response(data)
