3029292c83fefe5a2a3e1c075d12a7aa48d79ace
luto <m@luto.at>
2014-11-10 12:31:53 +0100

    don't give out user ids in shownoter list

--- snotes20/serializers/document.py
+++ snotes20/serializers/document.py
@@ -17,7 +17,7 @@ class DocumentMetaSerializer(ModelSerializer):
             meta = None
 
         if meta is not None:
-            data['shownoters'] = [{'name': shownoter.username, 'id': shownoter.id} for shownoter in meta.shownoters.all()]
+            data['shownoters'] = [{'name': shownoter.username} for shownoter in meta.shownoters.all()]
             data['podcasters'] = [{'name': rpodcaster.name} for rpodcaster in meta.podcasters.all()]
 
         return data
