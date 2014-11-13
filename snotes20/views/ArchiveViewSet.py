48d50f8ba04548731aa59899074ea22ddb52b361
luto <m@luto.at>
2014-11-13 23:11:47 +0100

    archive api: fix distinctness
    can't use .distinct() and .order_by() with different columns in django,
    so we resort to raw sql.

--- snotes20/views/ArchiveViewSet.py
+++ snotes20/views/ArchiveViewSet.py
@@ -9,15 +9,26 @@ import snotes20.serializers as serializers
 
 class ArchiveViewSet(viewsets.ViewSet):
     def list(self, request):
-        qry = models.Podcast.objects.filter(episodes__publications__isnull=False)
-
         type = 'all'
 
         if 'type' in request.QUERY_PARAMS:
             type = request.QUERY_PARAMS['type']
 
         if type == 'recent':
-            qry = qry.order_by('-episodes__publications__create_date')[:15]
+            qry = models.Podcast.objects.raw(
+                'SELECT DISTINCT ON ("id") * '
+                'FROM ('
+                '  SELECT "snotes20_podcast".*'
+                '  FROM "snotes20_podcast"'
+                '  INNER JOIN "snotes20_episode" ON ("snotes20_podcast"."id" = "snotes20_episode"."podcast_id")'
+                '  INNER JOIN "snotes20_publication" ON ("snotes20_episode"."id" = "snotes20_publication"."episode_id")'
+                '  WHERE "snotes20_publication"."id" IS NOT NULL'
+                '  ORDER BY "snotes20_publication"."create_date" DESC'
+                ') AS subb '
+                'LIMIT 15;'
+            )
+        else:
+            qry = models.Podcast.objects.filter(episodes__publications__isnull=False).distinct('id')
 
         data = serializers.PodcastSerializer(qry, many=True).data
 
