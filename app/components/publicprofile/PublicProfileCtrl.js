11484d9197eb5ed6f14463622c5faf64ed7de1cc
luto <m@luto.at>
2014-11-15 21:50:13 +0100

    fix public profile last online

--- app/components/publicprofile/PublicProfileCtrl.js
+++ app/components/publicprofile/PublicProfileCtrl.js
@@ -9,17 +9,17 @@ angular.module('snotes30App')
     user.get().then(function (user) {
       $scope.user = user;
 
-      var last_login = +new Date() - $scope.user.last_login;
+      var last_login = +new Date() - +new Date($scope.user.date_login);
       var day = 60 * 60 * 24 * 1000;
       var days = last_login / day;
 
-      if(days < 0) {
+      if(days <= 1) {
         $scope.last_login = "zuletzt heute";
-      } else if(days === 1) {
+      } else if(days <= 2) {
         $scope.last_login = "zuletzt gestern";
-      } else if(days === 2) {
+      } else if(days <= 3) {
         $scope.last_login = "zuletzt vorgestern";
-      } else if(days > 2 && days < 7) {
+      } else if(days > 3 && days < 7) {
         $scope.last_login = "zuletzt vor " + (~~days) + " Tagen";
       } else if(days >= 7 && days <= 10) {
         $scope.last_login = "zuletzt vor einer Woche";
