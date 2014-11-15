d1e1e34996505f48585ac1b0dea873bf081e5376
luto <m@luto.at>
2014-11-15 02:24:29 +0100

    refreshdocstate: allow all (not just recent) documents to be updated via cli

--- snotes20/management/commands/refreshdocstate.py
+++ snotes20/management/commands/refreshdocstate.py
@@ -31,25 +31,34 @@ def update_document(doc):
         doc.save()
 
 
+def update_documents(docs):
+    start_time = datetime.datetime.now()
+
+    for doc in docs:
+        logger.debug("Updating document:" + doc.name)
+        update_document(doc)
+
+    duration = datetime.datetime.now() - start_time
+
+    logger.debug("took {}s".format(duration.total_seconds()))
+
 
 class Command(BaseCommand):
     args = ''
     help = ''
 
     def handle(self, *args, **options):
+        mode = 'continuous'
 
-        while True:
-            start_time = datetime.datetime.now()
+        if len(args) == 1:
+            mode = args[0]
 
-            today = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
-            docs = models.Document.objects.all().filter(edit_date__gt=today)
-
-            for doc in docs:
-                logger.debug("Updating document:" + doc.name)
-                update_document(doc)
-
-            duration = datetime.datetime.now() - start_time
-
-            logger.debug("took {}s".format(duration.total_seconds()))
-
-            time.sleep(1)
+        if mode == 'continuous':
+            while True:
+                today = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
+                docs = models.Document.objects.all().filter(edit_date__gt=today)
+                update_documents(docs)
+                time.sleep(1)
+        elif mode == 'all':
+            docs = models.Document.objects.all()
+            update_documents(docs)
