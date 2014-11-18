8f312ad882519e2190486769ef9e63fd8a8ccf22
kaikubasta <hallo@kaikubasta.de>
2014-11-18 20:15:22 +0100

    revert config.js

--- app/components/config.js
+++ app/components/config.js
@@ -1,6 +1,6 @@
 angular.module('snotes30App').constant('CONFIG', {
-  apiBaseUrl: '/api',
-  mediaUrl: '/api/media/',
+  apiBaseUrl: 'http://snotes20.com:8000/',
+  mediaUrl: 'http://snotes20.com:8000/media/',
   websocket: {
     host: 'http://localhost:5133',
     path: '/'
