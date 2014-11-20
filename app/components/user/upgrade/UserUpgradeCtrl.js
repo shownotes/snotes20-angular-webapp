90b3504a62fc06b21cd166cbeb8c67a50ada19f9
luto <m@luto.at>
2014-11-20 16:49:42 +0100

    big nice yellow migrate warning

--- app/components/user/upgrade/UserUpgradeCtrl.js
+++ app/components/user/upgrade/UserUpgradeCtrl.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('snotes30App')
-  .controller('UserUpgradeCtrl', function ($scope, $location, Restangular, AuthenticationSvc) {
+  .controller('UserUpgradeCtrl', function ($scope, $rootScope, $location, Restangular, AuthenticationSvc) {
 
     AuthenticationSvc.injectMe($scope);
 
     $scope.doUpgrade = function () {
       Restangular.one('users', 'me').customPOST({password: $scope.upgrade.password}, "upgrade").then(function () {
+        AuthenticationSvc.injectMe($rootScope);
         $location.url('/');
       }, function () {
         alert("nope")
