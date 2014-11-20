d52c7aa2ee6d12096e714e383678dc20b8ae64e4
luto <m@luto.at>
2014-11-20 17:09:13 +0100

    user upgrade: proper feedback for submit

--- app/components/user/upgrade/UserUpgradeCtrl.js
+++ app/components/user/upgrade/UserUpgradeCtrl.js
@@ -1,16 +1,17 @@
 'use strict';
 
 angular.module('snotes30App')
-  .controller('UserUpgradeCtrl', function ($scope, $rootScope, $location, Restangular, AuthenticationSvc) {
+  .controller('UserUpgradeCtrl', function ($scope, $rootScope, $location, Restangular, AuthenticationSvc, actionStatusGlueService) {
+    $scope.upgradeStatus = {};
+    $scope.upgrade = {};
 
     AuthenticationSvc.injectMe($scope);
 
     $scope.doUpgrade = function () {
+      var stat = actionStatusGlueService.fac($scope.upgradeStatus);
       Restangular.one('users', 'me').customPOST({password: $scope.upgrade.password}, "upgrade").then(function () {
         AuthenticationSvc.injectMe($rootScope);
         $location.url('/');
-      }, function () {
-        alert("nope")
-      })
+      }, stat.reject);
     };
   });
