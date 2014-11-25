4fa5d4644ba1e8fea8fc02c40693c1f2e51800ba
luto <m@luto.at>
2014-11-25 15:00:45 +0100

    fix gruntfile jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -125,7 +125,7 @@ module.exports = function (grunt) {
             https: false,
             xforward: false,
             headers: {
-              "host": "new.shownot.es"
+              'host': 'new.shownot.es'
             }
           }
         ]
