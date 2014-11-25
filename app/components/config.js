a2c05bc84309e5c27c56f0c1690879fb753421be
luto <m@luto.at>
2014-11-25 15:01:20 +0100

    example config.js: remove useless slash at the end of media-url

--- app/components/config.js
+++ app/components/config.js
@@ -1,6 +1,6 @@
 angular.module('snotes30App').constant('CONFIG', {
   apiBaseUrl: 'http://snotes20.com:8000/',
-  mediaUrl: 'http://snotes20.com:8000/media/',
+  mediaUrl: 'http://snotes20.com:8000/media',
   websocket: {
     host: 'http://localhost:5133',
     path: '/'
