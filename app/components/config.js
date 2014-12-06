31c836fb0534b319db67f115665e0ba06b48b6c4
Dr4k3 <dr4k3@gmx.net>
2014-12-06 23:31:48 +0100

    other path

--- app/components/config.js
+++ app/components/config.js
@@ -2,7 +2,7 @@ angular.module('snotes30App').constant('CONFIG', {
   apiBaseUrl: 'http://snotes20.com:8000/',
   mediaUrl: 'http://snotes20.com:8000/media',
   websocket: {
-    host: 'http://localhost:5133',
+    host: 'http://snotes20.com:9999',
     path: '/'
   }
 });
