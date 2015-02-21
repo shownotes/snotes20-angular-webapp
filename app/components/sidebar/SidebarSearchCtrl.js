a83ac9c17bb96b52fc0e5d3e3432df54a08d33d6
Pascal Becker <pascal@beckerpascal.de>
2015-02-21 14:35:55 +0200

    Fixed typo: Crtl to Ctrl

--- app/components/sidebar/SidebarSearchCtrl.js
+++ app/components/sidebar/SidebarSearchCtrl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('snotes30App')
-  .controller('SidebarSearchCrtl', function ($scope, $location) {
+  .controller('SidebarSearchCtrl', function ($scope, $location) {
     $scope.submitSearch = function () {
       $location.url('/archive/search/?q=' + $scope.searchTerm);
     };
