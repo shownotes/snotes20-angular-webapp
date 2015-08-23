5cc01f8e2ea39c1c2a5f262bca040288387f5210
luto <m@luto.at>
2015-08-23 15:38:35 +0200

    fix editor URL in readonly view

--- app/components/document/readonly/DocumentReadonlyCtrl.js
+++ app/components/document/readonly/DocumentReadonlyCtrl.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('snotes30App')
-  .controller('DocumentReadonlyCtrl', function ($scope, $stateParams, $location, doc) {
+  .controller('DocumentReadonlyCtrl', function ($scope, $stateParams, $location, $state, doc) {
     $scope.doc = doc;
     $scope.episode = doc.episode;
     $scope.pub = $stateParams.pub;
-    $scope.currentUrl = $location.url();
+    $scope.docEditUrl = $state.href('document-edit', { name: doc.name }, {absolute: true});
 });
