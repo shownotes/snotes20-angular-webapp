969c67aa5daf4153f48bad97c749d04b94ba53ff
luto <m@luto.at>
2015-08-29 18:48:09 +0200

    use ngAnnotate instead of ngmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -353,10 +353,9 @@ module.exports = function (grunt) {
       }
     },
 
-    // ngmin tries to make the code safe for minification automatically by
-    // using the Angular long form for dependency injection. It doesn't work on
-    // things like resolve or inject so those have to be done manually.
-    ngmin: {
+    // ngAnnotate tries to make the code safe for minification automatically by
+    // using the Angular long form for dependency injection.
+    ngAnnotate: {
       dist: {
         files: [{
           expand: true,
@@ -539,7 +538,7 @@ module.exports = function (grunt) {
     'concurrent:dist',
     'autoprefixer',
     'concat',
-    'ngmin',
+    'ngAnnotate:dist',
     'copy:config_dist',
     'copy:dist',
     'cdnify',
