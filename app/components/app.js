3bb7c42c65ee40750f303192f05a10218182c577
bratwurscht <br4twurscht@gmail.com>
2016-01-03 17:31:25 +0100

    changed to StatisticsCtrl

--- app/components/app.js
+++ app/components/app.js
@@ -142,6 +142,7 @@ angular
       .state('statistics', {
         url: '/statistics/',
         templateUrl: 'components/statistics/statistics.html',
+        controller: 'StatisticsCtrl'
       })
       .state('faq', {
         url: '/faq/',
@@ -348,26 +349,4 @@ angular
     templateUrl: 'components/shared/loading.html',
     delay: 200,
     minDuration: 50
-  })
-.controller('MainCtrl', ['$scope', '$interval',
-    function ($scope, $interval) {
-      var time = new Date('2014-01-01 00:00:00 +0100');
-      // Random data point generator
-      var randPoint = function () {
-        var rand = Math.random;
-        return {time: new Date(time.toString()), visitors: rand() * 100};
-      }
-      // We store a list of logs
-      $scope.logs = [randPoint()];
-      $scope.moreLogs = [
-        [randPoint()], [randPoint()], [randPoint()], [randPoint()]
-      ];
-      $interval(function () {
-        time.setSeconds(time.getSeconds() + 1);
-        $scope.logs.push(randPoint());
-        $scope.moreLogs[0].push(randPoint());
-        $scope.moreLogs[1].push(randPoint());
-        $scope.moreLogs[2].push(randPoint());
-        $scope.moreLogs[3].push(randPoint());
-      }, 1000);
-    }]);
+  });
