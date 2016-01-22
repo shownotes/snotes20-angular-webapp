a9fc361ad3e14fa94b65a7940bde314d230235a1
bratwurscht <br4twurscht@gmail.com>
2016-01-23 00:22:51 +0100

    first step for rest

--- app/components/statistics/StatisticsCtrl.js
+++ app/components/statistics/StatisticsCtrl.js
@@ -1,28 +1,10 @@
 'use strict';
 
 angular.module('snotes30App')
-  .controller('StatisticsCtrl', ['$scope', '$interval',
-    function ($scope, $interval) {
-      var time = new Date('2014-01-01 00:00:00 +0100');
-      // Random data point generator
-      var randPoint = function () {
-        var rand = Math.random;
-        return {time: new Date(time.toString()), visitors: rand() * 100};
-      }
-      // We store a list of logs
-      $scope.logs = [randPoint()];
-      $scope.moreLogs = [
-        [randPoint()], [randPoint()], [randPoint()], [randPoint()]
-      ];
-      $interval(function () {
-        time.setSeconds(time.getSeconds() + 1);
-        $scope.logs.push(randPoint());
-        $scope.moreLogs[0].push(randPoint());
-        $scope.moreLogs[1].push(randPoint());
-        $scope.moreLogs[2].push(randPoint());
-        $scope.moreLogs[3].push(randPoint());
-      }, 1000);
+  .controller('StatisticsCtrl', ['$scope', 'Restangular',
+    function ($scope, Restangular) {
 
+      $scope.word = Restangular.all('statistic', 'wordfrequency', '?top=200').getList();
       $scope.words = ["Hallo", "Test", "Lorem", "Ipsum", "Lorem", "ipsum", "dolor", "sit", "amet,", "consetetur", "sadipscing", "elitr,", "sed", "diam", "nonumy", "eirmod", "tempor", "invidunt", "ut", "labore", "et", "dolore", "magna", "aliquyam", "erat,", "sed", "diam"];
 
       $scope.myOnClickFunction = function (element) {
