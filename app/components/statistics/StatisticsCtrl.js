508c37f0d88d7e43cd761976d0f98e8cfc8fcdf2
bratwurscht <br4twurscht@gmail.com>
2016-01-29 21:42:33 +0100

    works, diagramm doubled

--- app/components/statistics/StatisticsCtrl.js
+++ app/components/statistics/StatisticsCtrl.js
@@ -8,7 +8,7 @@ angular.module('snotes30App')
       });
 
       //$scope.words = ["Hallo", "Test", "Lorem", "Ipsum", "Lorem", "ipsum", "dolor", "sit", "amet,", "consetetur", "sadipscing", "elitr,", "sed", "diam", "nonumy", "eirmod", "tempor", "invidunt", "ut", "labore", "et", "dolore", "magna", "aliquyam", "erat,", "sed", "diam"];
-      $scope.words = ["holgi","toby","tim","nicolas","gibt","mehr","the","frage","mal","immer","apple","podcast","schon","macht","beim","geht","findet","gut","google","wurde","berlin","neue","holger","uhr","heute","menschen","2","of","frau","ja","kommt","–","album","chat","thema","herr","sendung","martinsen","'s","iphone","leute","musik","intro","twitter","ende","sendungsbeginn","gerne","max","shownotes","mac"];
+      //$scope.words = ["holgi","toby","tim","nicolas","gibt","mehr","the","frage","mal","immer","apple","podcast","schon","macht","beim","geht","findet","gut","google","wurde","berlin","neue","holger","uhr","heute","menschen","2","of","frau","ja","kommt","–","album","chat","thema","herr","sendung","martinsen","'s","iphone","leute","musik","intro","twitter","ende","sendungsbeginn","gerne","max","shownotes","mac"];
 
       $scope.myOnClickFunction = function (element) {
         console.log("click", element);
--- app/components/statistics/wordcloud/wordcloud-directive.js
+++ app/components/statistics/wordcloud/wordcloud-directive.js
@@ -4,117 +4,129 @@ angular.module('myChart')
   .directive('wordcloud', ['d3',function (d3) {
     return {
       restrict: 'E',
-        scope:{
-            // attributes
-            width : "@",
-            height: "@",
-            fontFamily : "@",
-            fontSize: "@",
+      scope:{
+          // attributes
+          width : "@",
+          height: "@",
+          fontFamily : "@",
+          fontSize: "@",
 
-            // Bindings
-            words: "=",
+          // Bindings
+          words: "=",
 
-            // EventCallbacks
-            onClick: "&",
-            onHover: "&"
+          // EventCallbacks
+          onClick: "&",
+          onHover: "&"
         },
-      link: function postLink(scope, element, attrs) {
+     link: function postLink(scope, element, attrs) {
+        scope.$watch('words',function() {
           // Default Values
-          var width             =   800;
-          var height            =   600;
-          var fontFamily        =   "Impact";
-          var fontSize          =   100;
+          var width = 800;
+          var height = 600;
+          var fontFamily = "Impact";
+          var fontSize = 100;
           var words;
 
           // Check and set attributes, else keep then default values
-          if(angular.isDefined(attrs.width))        width           = attrs.width;
-          if(angular.isDefined(attrs.height))       height          = attrs.height;
-          if(angular.isDefined(attrs.fontFamily))   fontFamily      = attrs.fontFamily;
-          if(angular.isDefined(attrs.fontSize))     fontSize        = attrs.fontSize * 1 || 0; // !parseInt, detect wrong input
+          if (angular.isDefined(attrs.width))        width = attrs.width;
+          if (angular.isDefined(attrs.height))       height = attrs.height;
+          if (angular.isDefined(attrs.fontFamily))   fontFamily = attrs.fontFamily;
+          if (angular.isDefined(attrs.fontSize))     fontSize = attrs.fontSize * 1 || 0; // !parseInt, detect wrong input
 
           // Check Scope
-          if(angular.isDefined(scope.words))    words   = scope.words;
+          if (angular.isDefined(scope.words))    words = scope.words;
 
 
           // Skip rendering when no corrent word param is parsed
-          if(angular.isDefined(scope.words) && angular.isArray(words)){
-              words   = scope.words
+          if (angular.isDefined(scope.words) && angular.isArray(words)) {
+            words = scope.words
           }
-          else if(angular.element(element).find("word").length>0){
-              var subelements = angular.element(element).find("word");
-              words = [];
-              angular.forEach(subelements,function(word){
-                  words.push(angular.element(word).text());
-                  angular.element(word).remove();
-              });
+          else if (angular.element(element).find("word").length > 0) {
+            var subelements = angular.element(element).find("word");
+            words = [];
+            angular.forEach(subelements, function (word) {
+              words.push(angular.element(word).text());
+              angular.element(word).remove();
+            });
           }
-          else if(element.text().length > 0){
-              words = element.text().split(",");
-              element.text("");
+          else if (element.text().length > 0) {
+            words = element.text().split(",");
+            element.text("");
 
           }
-          else{
-              element.text("wordcloud: Please define some words");
-              return;
+          else {
+            element.text("wordcloud: Please define some words");
+            return;
           }
           // Font-Size Param wrong
-          if(!angular.isNumber(fontSize) || fontSize<=0){
-              element.text("wordcloud: font-size attribute not valid. font-size "+attrs.fontSize +" -> "+fontSize);
-              return;
+          if (!angular.isNumber(fontSize) || fontSize <= 0) {
+            element.text("wordcloud: font-size attribute not valid. font-size " + attrs.fontSize + " -> " + fontSize);
+            return;
           }
 
           // CloudFactory - so high in the sky ;)
           // Keep the anonym functions here for readability
-          var cloudFactory = function(words){
+          var cloudFactory = function (words) {
 
 
-              var fill = d3.scale.category20();
+            var fill = d3.scale.category20();
 
-              d3.layout.cloud().size([width, height])
-                  .words(words.map(function(d) {
-                      return {text: d, size: Math.random() * fontSize};
-                  }))
-                  .rotate(function() { return ~~(Math.random() * 2) * -90; })
-                  .font(fontFamily)
-                  .fontSize(function(d) { return d.size; })
-                  .on("end", draw)
-                  .start();
+            d3.layout.cloud().size([width, height])
+              .words(words.map(function (d) {
+                return {text: d, size: Math.random() * fontSize};
+              }))
+              .rotate(function () {
+                return ~~(Math.random() * 2) * -90;
+              })
+              .font(fontFamily)
+              .fontSize(function (d) {
+                return d.size;
+              })
+              .on("end", draw)
+              .start();
 
-              function draw(words) {
-                  // Center the drawing
-                  var height_translate = height/2;
-                  var width_translate = width/2;
-                  var rootElement = element[0];
+            function draw(words) {
+              // Center the drawing
+              var height_translate = height / 2;
+              var width_translate = width / 2;
+              var rootElement = element[0];
 
 
-                  d3.select(rootElement)
-                      .append("svg")
-                      .attr("width", width)
-                      .attr("height", height)
-                      .append("g")
-                      .attr("transform", "translate("+width_translate+","+height_translate+")")// Translate to center
-                      .selectAll("text")
-                      .data(words)
-                      .enter().append("text")
-                      .style("font-size", function(d) { return d.size + "px"; })
-                      .style("font-family", fontFamily)
-                      .style("fill", function(d, i) { return fill(i); })
-                      .attr("text-anchor", "middle")
-                      .attr("transform", function(d) {
-                          return "translate(" + [d.x, d.y] + ") rotate(" + d.rotate + ")";
-                      })
-                      .text(function(d) { return d.text; })
-                      .on("click",function(d){
-                          scope.onClick({element: d});
-                      })
-                      .on("mouseover",function(d){
-                          scope.onHover({element: d});
-                      });
-              }
+              d3.select(rootElement)
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height)
+                .append("g")
+                .attr("transform", "translate(" + width_translate + "," + height_translate + ")")// Translate to center
+                .selectAll("text")
+                .data(words)
+                .enter().append("text")
+                .style("font-size", function (d) {
+                  return d.size + "px";
+                })
+                .style("font-family", fontFamily)
+                .style("fill", function (d, i) {
+                  return fill(i);
+                })
+                .attr("text-anchor", "middle")
+                .attr("transform", function (d) {
+                  return "translate(" + [d.x, d.y] + ") rotate(" + d.rotate + ")";
+                })
+                .text(function (d) {
+                  return d.text;
+                })
+                .on("click", function (d) {
+                  scope.onClick({element: d});
+                })
+                .on("mouseover", function (d) {
+                  scope.onHover({element: d});
+                });
+            }
           };
 
           // Execute
           cloudFactory(words);
+        });
       }
     };
   }]);
