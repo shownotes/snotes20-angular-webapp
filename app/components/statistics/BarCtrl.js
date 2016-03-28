7c3533ee52d518523939c2d830aadf3489fd16f0
Felipe Paiva <fdsp84@gmail.com>
2016-03-28 17:44:31 +0200

    adding dummy bar chart in the target pages

--- app/components/statistics/BarCtrl.js
+++ app/components/statistics/BarCtrl.js
@@ -4,15 +4,8 @@ angular.module("snotes30App")
   .controller("BarCtrl", function ($scope) {
 
     $scope.chart = {
-    	labels : ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+    	labels : ["09.2015", "12.2015", "01.2016", "02.2106", "03.2016"],
      	datasets : [
-        	{
-                	fillColor : "rgba(151,187,205,0)",
-            		strokeColor : "#e67e22",
-	                pointColor : "rgba(151,187,205,0)",
-            		pointStrokeColor : "#e67e22",
-            		data : [4, 3, 5, 4, 6]
-        	},
         	{
                    	fillColor : "rgba(151,187,205,0)",
             		strokeColor : "#f1c40f",
